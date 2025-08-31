2337d041a6d3436fe6c665047103efe324763636
Ritvik Rajkumar <spatialcurrents@gmail.com>
2025-08-31 15:37:34 +0000

    Console Error
    FirebaseError: Firebase: Error (auth/configuration-not-f

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "nutritrackgo-nvldr",
-  "appId": "1:223092470506:web:49ae84375bd29b43939d93",
-  "storageBucket": "nutritrackgo-nvldr.firebasestorage.app",
-  "apiKey": "AIzaSyBRtns8f4qKFlFXX4_rL_t4fA8h3v3JnP4",
-  "authDomain": "nutritrackgo-nvldr.firebaseapp.com",
-  "messagingSenderId": "223092470506",
-  "measurementId": ""
+  projectId: "nutritrackgo-nvldr",
+  appId: "1:223092470506:web:49ae84375bd29b43939d93",
+  storageBucket: "nutritrackgo-nvldr.firebasestorage.app",
+  apiKey: "AIzaSyBRtns8f4qKFlFXX4_rL_t4fA8h3v3JnP4",
+  authDomain: "nutritrackgo-nvldr.firebaseapp.com",
+  messagingSenderId: "223092470506",
+  measurementId: ""
 };
 
 // Initialize Firebase
